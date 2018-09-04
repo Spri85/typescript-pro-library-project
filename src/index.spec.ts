d4de43f3c6ff87e4b98600f76075c944ae65955e
Viktor <viktorpar@gmail.com>
2018-09-04 10:54:14 +0300

    configure lint-staged

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,6 @@
-const lib = require('./index');
+import lib from './index'
+
 test('It should work', () => {
-  expect(true).toEqual(true);
-  console.log(lib);
-});
+  expect(true).toEqual(true)
+  console.log(lib)
+})
